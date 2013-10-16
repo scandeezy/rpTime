2ae4b06b7458f06382ac2bfdccf314d0439c7eae
JJ Zabkar <jjzabkar@gmail.com>
2013-10-16 13:51:38 -0700

    Updated marshalling/unmarshalling for `CompanyService`.

--- src/main/java/com/roosterpark/rptime/service/CompanyService.java
+++ src/main/java/com/roosterpark/rptime/service/CompanyService.java
@@ -13,78 +13,73 @@ import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Query;
 import com.roosterpark.rptime.model.Company;
 
-public class CompanyService implements Service<Company>
-{
+public class CompanyService implements Service<Company> {
 	public static final String COMPANY_BUCKET_KEY = "companies";
 	public static final String COMPANY_NAME_KEY = "companyName";
 	public static final String COMPANY_HEADER_KEY = "companyHeader";
 	public static final String COMPANY_PHONE_KEY = "companyPhone";
-	public static final String COMPANY_START_KEY = "companyStartDay";
+	public static final String COMPANY_START_DAY_OF_WEEK_KEY = "companyStartDay";
 
 	@Override
-	public Company get(String sKey) throws EntityNotFoundException
-	{
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        Key key = KeyFactory.createKey(COMPANY_BUCKET_KEY, sKey);
-        Entity entity;
-        entity = datastore.get(key);
-        Company company = toCompany(entity);
-        
+	public Company get(String sKey) throws EntityNotFoundException {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Key key = KeyFactory.createKey(COMPANY_BUCKET_KEY, sKey);
+		Entity entity;
+		entity = datastore.get(key);
+		Company company = toCompany(entity);
+
 		return company;
 	}
-	
+
 	@Override
-	public List<Company> getPage(int count, int offset)
-	{
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        Query query = new Query(COMPANY_BUCKET_KEY);
+	public List<Company> getPage(int count, int offset) {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query query = new Query(COMPANY_BUCKET_KEY);
 		List<Entity> entities = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(count).offset(offset));
-		
+
 		List<Company> companies = new LinkedList<Company>();
-		for(Entity entity : entities)
-		{
+		for (Entity entity : entities) {
 			companies.add(toCompany(entity));
 		}
-		
+
 		return companies;
 	}
 
 	@Override
-	public void set(Company item) 
-	{
+	public void set(Company item) {
 		Entity company = toEntity(item);
-		
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        datastore.put(company);
+
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		datastore.put(company);
 	}
-	
+
 	/**
 	 * Dumb mappers
+	 * 
 	 * @param input
 	 * @return
 	 */
-	
-	public Entity toEntity(Company input)
-	{
-		if(input == null)
+
+	public Entity toEntity(Company input) {
+		if (input == null)
 			throw new IllegalArgumentException("No arguments may be null.");
 		Entity company = new Entity(COMPANY_BUCKET_KEY);
-		
+
 		company.setProperty(COMPANY_NAME_KEY, input.getName());
 		company.setProperty(COMPANY_HEADER_KEY, input.getHeader());
 		company.setProperty(COMPANY_PHONE_KEY, input.getPhone());
-		
+		company.setProperty(COMPANY_START_DAY_OF_WEEK_KEY, input.getStartDayOfWeek());
+
 		return company;
 	}
-	
-	public Company toCompany(Entity entity)
-	{
+
+	public Company toCompany(Entity entity) {
 		Company company = new Company();
-		company.setName((String)entity.getProperty(COMPANY_NAME_KEY));
-		company.setHeader((String)entity.getProperty(COMPANY_NAME_KEY));
-		company.setPhone((String)entity.getProperty(COMPANY_NAME_KEY));
-		company.setStartDayOfWeek((Integer)entity.getProperty(COMPANY_PHONE_KEY));
-		
+		company.setName((String) entity.getProperty(COMPANY_NAME_KEY));
+		company.setHeader((String) entity.getProperty(COMPANY_HEADER_KEY));
+		company.setPhone((String) entity.getProperty(COMPANY_PHONE_KEY));
+		company.setStartDayOfWeek((Integer) entity.getProperty(COMPANY_START_DAY_OF_WEEK_KEY));
+
 		return company;
 	}
 }
