36c042999b9612f135ce76f70a0cd301a6e9b715
JJ Zabkar <jjzabkar@gmail.com>
2013-10-22 15:23:46 -0700

    Updated RESTful `Client` MVC resources.

--- src/main/java/com/roosterpark/rptime/service/ClientService.java
+++ src/main/java/com/roosterpark/rptime/service/ClientService.java
@@ -4,16 +4,23 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
 
 import java.util.List;
 
-import javax.inject.Singleton;
+import javax.inject.Named;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.googlecode.objectify.ObjectifyService;
 import com.roosterpark.rptime.model.Client;
 
-@Singleton
+@Named
 public class ClientService {
 	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+	public ClientService() {
+		LOGGER.trace("registering Company class with ObjectifyService");
+		ObjectifyService.register(Client.class);
+		LOGGER.trace("registered Company");
+	}
 
 	public Client getById(Long id) {
 		LOGGER.warn("Getting company with key " + id);
@@ -24,7 +31,7 @@ public class ClientService {
 		LOGGER.warn("Getting company page with count " + count + " and offset " + offset);
 		return ofy().load().type(Client.class).limit(count).offset(offset).list();
 	}
-	
+
 	public List<Client> getAll() {
 		return ofy().load().type(Client.class).list();
 	}
