0e2b1d2e3b6bb89a1fe86044890cbc2a04385dc9
JJ Zabkar <jjzabkar@gmail.com>
2013-11-05 17:14:45 -0800

    Complete: Report Total Hours Per Worker Per Month (across clients)

--- src/main/java/com/roosterpark/rptime/service/ReportService.java
+++ src/main/java/com/roosterpark/rptime/service/ReportService.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.joda.time.Interval;
 import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,42 +38,39 @@ public class ReportService {
 		final Map<Long, Long> result = new LinkedHashMap<Long, Long>();
 		LocalDate date1 = new LocalDate(d.getYear(), d.getMonthOfYear(), 1);
 		LocalDate date2 = new LocalDate(date1.plusMonths(1));
-		Interval i = new Interval(date1.toDateTimeAtStartOfDay(), date2.toDateTimeAtStartOfDay());
 
-		LOGGER.debug("building report for {} workers, {} timeSheets", workers.size(), timeSheets.size());
-
+		LOGGER.debug("building report for {} workers, {} timeSheets with dates betwen [{},{}]",
+				new Object[] { workers.size(), timeSheets.size(), date1, date2 });
 		for (Worker w : workers) {
 			Long workerId = w.getId();
-			LOGGER.debug("\t found {} timesheets for worker {}", timeSheets.size(), workerId);
-
-			Long hours = new Long(0L); // TODO: calc
-
+			LOGGER.trace("\t found {} timesheets for worker {}", timeSheets.size(), workerId);
+			Long hours = new Long(0L);
 			for (TimeSheetView t : timeSheets) {
-
-				List<TimeSheetDay> days = t.getDays();
-				LOGGER.debug("\t\t found {} days for timeSheet {}", days.size(), t.getId());
-
-				List<TimeCardLogEntry> entries;
-				for (TimeSheetDay day : days) {
-					entries = day.getEntries();
-					LOGGER.debug("\t\t found {} entries for timeSheet {}", entries.size(), t.getId());
-					for (TimeCardLogEntry entry : entries) {
-						LocalDate entryDate = entry.getDate();
-						LOGGER.debug("\t\t\tchecking if {} < {} < {} ", new Object[] { date1, entryDate, date2 });
-						if (date1.isBefore(entryDate) && date2.isAfter(entryDate)) {
-							int start = entry.getStartTime().getMillisOfDay();
-							int end = entry.getEndTime().getMillisOfDay();
-							long entryMillis = (long) (end - start); // millis
-							long entryHours = entryMillis / (60L * 60L * 1000L); // millis
-							hours = hours + entryHours;
+				if (workerId.equals(t.getWorkerId())) {
+					List<TimeSheetDay> days = t.getDays();
+					LOGGER.trace("\t\t found {} days for timeSheet {}", days.size(), t.getId());
+					List<TimeCardLogEntry> entries;
+					for (TimeSheetDay day : days) {
+						entries = day.getEntries();
+						LOGGER.trace("\t\t found {} entries for timeSheet {}", entries.size(), t.getId());
+						for (TimeCardLogEntry entry : entries) {
+							LocalDate entryDate = entry.getDate();
+							LOGGER.trace("\t\t\tchecking if {} < {} < {} ", new Object[] { date1, entryDate, date2 });
+							if (date1.isBefore(entryDate) //
+									&& date2.isAfter(entryDate) //
+									&& entry.getStartTime() != null //
+									&& entry.getEndTime() != null) {
+								int start = entry.getStartTime().getMillisOfDay();
+								int end = entry.getEndTime().getMillisOfDay();
+								long entryMillis = (long) (end - start); // millis
+								long entryHours = entryMillis / (60L * 60L * 1000L);
+								hours = hours + entryHours;
+							}
 						}
 					}
-
 				}
-
 			}
-
-			LOGGER.debug("\t put {} hours at worker {}", hours, workerId);
+			LOGGER.trace("\t put {} hours at worker {}", hours, workerId);
 			result.put(workerId, hours);
 		}
 		return result;
