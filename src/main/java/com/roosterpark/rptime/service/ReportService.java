3a61655b4551389af75df148003627cf2aba208e
JJ Zabkar <jjzabkar@gmail.com>
2013-11-05 16:38:16 -0800

    In progress: Report Total Hours Per Worker Per Month.  Resolving issues with shape of TimeSheet object graph.

--- src/main/java/com/roosterpark/rptime/service/ReportService.java
+++ src/main/java/com/roosterpark/rptime/service/ReportService.java
@@ -8,11 +8,14 @@ import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.joda.time.Interval;
 import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.roosterpark.rptime.model.TimeSheet;
+import com.roosterpark.rptime.model.TimeCardLogEntry;
+import com.roosterpark.rptime.model.TimeSheetDay;
+import com.roosterpark.rptime.model.TimeSheetView;
 import com.roosterpark.rptime.model.Worker;
 
 /**
@@ -31,25 +34,42 @@ public class ReportService {
 
 	private String yearMonthDateTimeFormatter = "MMMM YYYY";
 
-	protected Map<Long, Long> getWorkerIdToHoursMapForMonth(final List<Worker> workers, final LocalDate d) {
+	protected Map<Long, Long> getWorkerIdToHoursMapForMonth(final List<Worker> workers, final List<TimeSheetView> timeSheets,
+			final LocalDate d) {
 		final Map<Long, Long> result = new LinkedHashMap<Long, Long>();
+		LocalDate date1 = new LocalDate(d.getYear(), d.getMonthOfYear(), 1);
+		LocalDate date2 = new LocalDate(date1.plusMonths(1));
+		Interval i = new Interval(date1.toDateTimeAtStartOfDay(), date2.toDateTimeAtStartOfDay());
 
-		LOGGER.debug("found {} workers for date {}", workers.size(), d);
+		LOGGER.debug("building report for {} workers, {} timeSheets", workers.size(), timeSheets.size());
 
 		for (Worker w : workers) {
 			Long workerId = w.getId();
-			List<TimeSheet> timeSheets = timeSheetService.getAllForWorker(workerId);
-
 			LOGGER.debug("\t found {} timesheets for worker {}", timeSheets.size(), workerId);
 
-			Long hours = new Long(40L); // TODO: calc
+			Long hours = new Long(0L); // TODO: calc
 
-			for (TimeSheet t : timeSheets) {
-				List<Long> ids = t.getTimeCardIds();
-				LOGGER.debug("\t\t found {} timeCardIds for timeSheet {}", ids.size(), t.getId());
+			for (TimeSheetView t : timeSheets) {
 
-				for (Long id : ids) {
-					hours = hours + 1L; // TODO: FIXME
+				List<TimeSheetDay> days = t.getDays();
+				LOGGER.debug("\t\t found {} days for timeSheet {}", days.size(), t.getId());
+
+				List<TimeCardLogEntry> entries;
+				for (TimeSheetDay day : days) {
+					entries = day.getEntries();
+					LOGGER.debug("\t\t found {} entries for timeSheet {}", entries.size(), t.getId());
+					for (TimeCardLogEntry entry : entries) {
+						LocalDate entryDate = entry.getDate();
+						LOGGER.debug("\t\t\tchecking if {} < {} < {} ", new Object[] { date1, entryDate, date2 });
+						if (date1.isBefore(entryDate) && date2.isAfter(entryDate)) {
+							int start = entry.getStartTime().getMillisOfDay();
+							int end = entry.getEndTime().getMillisOfDay();
+							long entryMillis = (long) (end - start); // millis
+							long entryHours = entryMillis / (60L * 60L * 1000L); // millis
+							hours = hours + entryHours;
+						}
+					}
+
 				}
 
 			}
@@ -64,8 +84,9 @@ public class ReportService {
 		final Map<String, Object> map = new HashMap<String, Object>();
 		final LocalDate d = new LocalDate();
 		List<Worker> workers = workerService.getAll();
+		List<TimeSheetView> timeSheets = timeSheetService.getAll();
 		map.put("workerList", workers);
-		map.put("workerIdToHoursMap", getWorkerIdToHoursMapForMonth(workers, d));
+		map.put("workerIdToHoursMap", getWorkerIdToHoursMapForMonth(workers, timeSheets, d));
 		map.put("reportDate", d.toString(yearMonthDateTimeFormatter));
 		return map;
 	}
--- src/main/java/com/roosterpark/rptime/service/TimeSheetService.java
+++ src/main/java/com/roosterpark/rptime/service/TimeSheetService.java
@@ -1,10 +1,5 @@
 package com.roosterpark.rptime.service;
 
-import com.googlecode.objectify.Key;
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,7 +8,6 @@ import javax.inject.Named;
 import javax.persistence.EntityNotFoundException;
 
 import org.joda.time.LocalDate;
-import org.joda.time.LocalDateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,22 +25,22 @@ public class TimeSheetService {
 
 	@Inject
 	ContractService contractService;
-        
-        @Inject
-        TimeSheetDao timeSheetDao;
-        
-        @Inject
-        TimeSheetDayDao timeSheetDayDao;
+
+	@Inject
+	TimeSheetDao timeSheetDao;
+
+	@Inject
+	TimeSheetDayDao timeSheetDayDao;
 
 	public List<TimeSheetView> createForWorkerDate(Long workerId, LocalDate date) {
 		List<Contract> contracts = contractService.getActiveContractsForWorker(workerId, date);
 		if (!contracts.isEmpty()) {
-                        List<TimeSheetView> views = new LinkedList<>();
-                        TimeSheetView view = new TimeSheetView();
+			List<TimeSheetView> views = new LinkedList<>();
+			TimeSheetView view = new TimeSheetView();
 			List<TimeSheet> sheets = new LinkedList<>();
 
 			for (Contract contract : contracts) {
-                            views.add(createForWorkerDateContract(workerId, date, contract));
+				views.add(createForWorkerDateContract(workerId, date, contract));
 			}
 
 			return views;
@@ -56,110 +50,105 @@ public class TimeSheetService {
 				+ "'.  Solution: create Contract on the /contracts page for this Worker with beginDate < " + date + "< endDate.");
 
 	}
-        
-        public TimeSheetView createForWorkerDateContract(Long workerId, LocalDate date, Contract contract) {
-                // TODO verify this sheet doesn't already exist.
-                LOGGER.debug("created new TimeSheet for worker={}, date={}, contract={}", workerId, date, contract);
-                
-                List<Long> logIds = new LinkedList<>();
-                List<TimeSheetDay> entries = new LinkedList<>();
-                for(int i = 0; i < 7; i++) {
-                    TimeSheetDay day = new TimeSheetDay();
-                    TimeCardLogEntry entry = new TimeCardLogEntry(workerId, contract.getClient(), date.plusDays(i));
-                    // Retain the ID
-                    day.addEntry(entry);
-                    day = timeSheetDayDao.set(day);
-                    entries.add(day);
-                    logIds.add(day.getId());
-                }
-                Long clientId = contract.getClient();
-                LocalDate contractDate = adjustDate(date, contract.getStartDayOfWeek());
-                TimeSheet result = new TimeSheet(workerId, clientId, contractDate, logIds);
 
-                result = timeSheetDao.set(result);
-                
-                TimeSheetView view = new TimeSheetView(result,entries);
-                
-                return view;
-        }
-        
-        public List<TimeSheetView> getSheetViewsForWorkerPage(Long workerId, Integer count, Integer offset) {
-                List<TimeSheet> sheets = timeSheetDao.getSheetViewsForWorkerPage(workerId, count, offset);
-                List<TimeSheetView> views = new LinkedList<>();
-                for(TimeSheet sheet : sheets) {
-                        views.add(convert(sheet));
-                }
+	public TimeSheetView createForWorkerDateContract(Long workerId, LocalDate date, Contract contract) {
+		// TODO verify this sheet doesn't already exist.
+		LOGGER.debug("created new TimeSheet for worker={}, date={}, contract={}", workerId, date, contract);
 
-                return views;
-        }
-        
-        private TimeSheetView convert(TimeSheet sheet) {
-                List<TimeSheetDay> entries = timeSheetDayDao.getEntries(sheet.getTimeCardIds());
-                // TODO assert the order is consistent
-                return new TimeSheetView(sheet, entries);
-        }
-        
-        private LocalDate adjustDate(LocalDate date, Integer dayOfWeek) {
-            int currentDayOfWeek = date.dayOfWeek().get();
-            LOGGER.debug("Current day of week {} and needed day of week {}", currentDayOfWeek, dayOfWeek);
-            
-            return date.plusDays(dayOfWeek - currentDayOfWeek);
-        }
+		List<Long> logIds = new LinkedList<>();
+		List<TimeSheetDay> entries = new LinkedList<>();
+		for (int i = 0; i < 7; i++) {
+			TimeSheetDay day = new TimeSheetDay();
+			TimeCardLogEntry entry = new TimeCardLogEntry(workerId, contract.getClient(), date.plusDays(i));
+			// Retain the ID
+			day.addEntry(entry);
+			day = timeSheetDayDao.set(day);
+			entries.add(day);
+			logIds.add(day.getId());
+		}
+		Long clientId = contract.getClient();
+		LocalDate contractDate = adjustDate(date, contract.getStartDayOfWeek());
+		TimeSheet result = new TimeSheet(workerId, clientId, contractDate, logIds);
 
-//	public List<TimeSheet> getAllForWorker(Long workerId) {
-//		LOGGER.warn("Getting TimeSheets for workerId={}", workerId);
-//		return ofy().load().type(TimeSheet.class).filter("workerId", workerId).list();
-//	}
-//
+		result = timeSheetDao.set(result);
+
+		TimeSheetView view = new TimeSheetView(result, entries);
+
+		return view;
+	}
+
+	public List<TimeSheetView> getSheetViewsForWorkerPage(Long workerId, Integer count, Integer offset) {
+		List<TimeSheet> sheets = timeSheetDao.getSheetViewsForWorkerPage(workerId, count, offset);
+		List<TimeSheetView> views = new LinkedList<>();
+		for (TimeSheet sheet : sheets) {
+			views.add(convert(sheet));
+		}
+
+		return views;
+	}
+
+	private TimeSheetView convert(TimeSheet sheet) {
+		List<TimeSheetDay> entries = timeSheetDayDao.getEntries(sheet.getTimeCardIds());
+		// TODO assert the order is consistent
+		return new TimeSheetView(sheet, entries);
+	}
+
+	private LocalDate adjustDate(LocalDate date, Integer dayOfWeek) {
+		int currentDayOfWeek = date.dayOfWeek().get();
+		LOGGER.debug("Current day of week {} and needed day of week {}", currentDayOfWeek, dayOfWeek);
+
+		return date.plusDays(dayOfWeek - currentDayOfWeek);
+	}
+
 	public List<TimeSheetView> getAll() {
 		LOGGER.warn("Getting TimeSheets for admin");
 		List<TimeSheet> sheets = timeSheetDao.getAll();
-                LOGGER.debug("Found these sheets for all {}", sheets);
-                List<TimeSheetView> views = new LinkedList<>();
-                for(TimeSheet sheet : sheets) {
-                    views.add(convert(sheet));
-                }
-                
-                return views;
+		LOGGER.debug("Found these sheets for all {}", sheets);
+		List<TimeSheetView> views = new LinkedList<>();
+		for (TimeSheet sheet : sheets) {
+			views.add(convert(sheet));
+		}
+
+		return views;
 	}
 
 	public TimeSheetView getById(Long id) {
-                TimeSheet sheet = timeSheetDao.getById(id);
-                
-                return convert(sheet);
+		TimeSheet sheet = timeSheetDao.getById(id);
+
+		return convert(sheet);
 	}
 
-//	public List<TimeSheet> getClientWeekPage(String client, Integer week, int count, int offset) {
-//		return ofy().load().type(TimeSheet.class).filter(TimeSheet.CLIENT_KEY, client).filter(TimeSheet.WEEK_KEY, week).limit(count)
-//				.offset(offset).list();
-//	}
-//
-//	public List<TimeSheet> getWorkerWeekPage(String worker, Integer week, int count, int offset) {
-//		return ofy().load().type(TimeSheet.class).filter(TimeSheet.WORKER_KEY, worker).filter(TimeSheet.WEEK_KEY, week).limit(count)
-//				.offset(offset).list();
-//	}
-//
-//	public List<TimeSheet> getPage(int count, int offset) {
-//		return ofy().load().type(TimeSheet.class).limit(count).offset(offset).list();
-//	}
+	// public List<TimeSheet> getClientWeekPage(String client, Integer week, int count, int offset) {
+	// return ofy().load().type(TimeSheet.class).filter(TimeSheet.CLIENT_KEY, client).filter(TimeSheet.WEEK_KEY, week).limit(count)
+	// .offset(offset).list();
+	// }
+	//
+	// public List<TimeSheet> getWorkerWeekPage(String worker, Integer week, int count, int offset) {
+	// return ofy().load().type(TimeSheet.class).filter(TimeSheet.WORKER_KEY, worker).filter(TimeSheet.WEEK_KEY, week).limit(count)
+	// .offset(offset).list();
+	// }
+	//
+	// public List<TimeSheet> getPage(int count, int offset) {
+	// return ofy().load().type(TimeSheet.class).limit(count).offset(offset).list();
+	// }
 
-        public TimeSheetView set(TimeSheetView view) {
-                LOGGER.debug("Saving timesheet {}", view);
-                List<TimeSheetDay> entries = view.getDays();
-                entries = timeSheetDayDao.set(entries);
-                view.setDays(entries);
-                TimeSheet sheet = new TimeSheet(view);
-                sheet = timeSheetDao.set(sheet);
-                
-                return new TimeSheetView(sheet, entries);
-        }
+	public TimeSheetView set(TimeSheetView view) {
+		LOGGER.debug("Saving timesheet {}", view);
+		List<TimeSheetDay> entries = view.getDays();
+		entries = timeSheetDayDao.set(entries);
+		view.setDays(entries);
+		TimeSheet sheet = new TimeSheet(view);
+		sheet = timeSheetDao.set(sheet);
+
+		return new TimeSheetView(sheet, entries);
+	}
 
 	public void delete(Long id) {
 		TimeSheet c = timeSheetDao.getById(id);
-                List<TimeSheetDay> entries = timeSheetDayDao.getEntries(c.getTimeCardIds());
-                
-                timeSheetDayDao.delete(c.getTimeCardIds());
-                timeSheetDao.delete(c.getId());
+		List<TimeSheetDay> entries = timeSheetDayDao.getEntries(c.getTimeCardIds());
+
+		timeSheetDayDao.delete(c.getTimeCardIds());
+		timeSheetDao.delete(c.getId());
 	}
 
 }
