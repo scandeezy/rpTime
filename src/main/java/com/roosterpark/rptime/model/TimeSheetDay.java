d4faf7d200020c9cccf5a62a35d06dae6fd90426
JJ Zabkar <jjzabkar@gmail.com>
2013-11-19 13:55:59 -0800

    Fix for NPE/AOB error.

--- src/main/java/com/roosterpark/rptime/model/TimeSheetDay.java
+++ src/main/java/com/roosterpark/rptime/model/TimeSheetDay.java
@@ -4,55 +4,52 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
+
 /**
- *
+ * 
  * @author scandeezy
  */
-public class TimeSheetDay
-{
-    private Long id;
-    private List<TimeCardLogEntry> entries;
-    
-    public TimeSheetDay() {
-        this.entries = new LinkedList<>();
-    }
-    
-    public TimeSheetDay(List<TimeCardLogEntry> entries) {
-        this.entries = entries;
-        this.id = entries.get(0).getCardId();
-    }
+public class TimeSheetDay {
+	private Long id;
+	private List<TimeCardLogEntry> entries;
 
-    public Long getId()
-    {
-        return id;
-    }
+	public TimeSheetDay() {
+		this.entries = new LinkedList<>();
+	}
 
-    public void setId(Long id)
-    {
-        this.id = id;
-    }
-    
-    public List<TimeCardLogEntry> getEntries()
-    {
-        return entries;
-    }
-    
-    public void addEntry(TimeCardLogEntry entry) {
-        this.entries.add(entry);
-        Collections.sort(entries);
-    }
+	public TimeSheetDay(List<TimeCardLogEntry> entries) {
+		this.entries = entries;
+		if (CollectionUtils.isNotEmpty(entries)) {
+			this.id = entries.get(0).getCardId();
+		} else {
+			this.id = new Long((long) (Math.random() * 1111111L));
+		}
+	}
 
-    public void setEntries(List<TimeCardLogEntry> entries)
-    {
-        this.entries = entries;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    @Override
-    public String toString()
-    {
-        return "TimeSheetDay{" 
-                + "id=" + id 
-                + ", entries=" + entries 
-                + '}';
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<TimeCardLogEntry> getEntries() {
+		return entries;
+	}
+
+	public void addEntry(TimeCardLogEntry entry) {
+		this.entries.add(entry);
+		Collections.sort(entries);
+	}
+
+	public void setEntries(List<TimeCardLogEntry> entries) {
+		this.entries = entries;
+	}
+
+	@Override
+	public String toString() {
+		return "TimeSheetDay{" + "id=" + id + ", entries=" + entries + '}';
+	}
 }
