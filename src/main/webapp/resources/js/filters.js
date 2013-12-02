ac9ad53d4eff4a2d3121fa68fcdf083539783ecb
JJ Zabkar <jjzabkar@gmail.com>
2013-12-02 12:08:35 -0800

    Added timestamp fomatting filter.

--- src/main/webapp/resources/js/filters.js
+++ src/main/webapp/resources/js/filters.js
@@ -1,34 +1,44 @@
 'use strict';
 (function() {
-	
-var module = angular.module('myApp.filters', []);
 
-module.filter('interpolate', ['version', function(version) { //
-    return function interpolateFn(text) {
-        return String(text).replace(/\%VERSION\%/mg, version);
-      };
-}]);
+	var module = angular.module('myApp.filters', []);
 
-module.value('dayOfWeekArr',
-// Preferred:
-//		{ //
-// 	"0" : { code: "0", description: "Sunday" }, 
-// 	"1" : { code: "1", description: "Monday" },
-// 	"2" : { code: "2", description: "Tuesday" },
-// 	"3" : { code: "3", description: "Wednesday" },
-// 	"4" : { code: "4", description: "Thursday" },
-// 	"5" : { code: "5", description: "Friday" },
-// 	"6" : { code: "6", description: "Saturday" }
-//}
-	[	
-	 	{ code: 0, description: "Sunday" }, 
-	 	{ code: 1, description: "Monday" },
-	 	{ code: 2, description: "Tuesday" },
-	 	{ code: 3, description: "Wednesday" },
-	 	{ code: 4, description: "Thursday" },
-	 	{ code: 5, description: "Friday" },
-	 	{ code: 6, description: "Saturday" }
-	]
-);
+	module.filter('interpolate', [ 'version', function(version) { //
+		return function interpolateFn(text) {
+			return String(text).replace(/\%VERSION\%/mg, version);
+		};
+	} ]);
+
+	module.filter('truncatedtimestamp', function truncatedtimestampFilterFn() { //
+		return function truncatedtimestampFilterFnFn(text) {
+			if (text && text.length == 23) {
+				return String(text).substr(0, 16);
+			}
+			return text;
+		};
+	} );
+
+	module.value('dayOfWeekArr', [ {
+		code : 0,
+		description : "Sunday"
+	}, {
+		code : 1,
+		description : "Monday"
+	}, {
+		code : 2,
+		description : "Tuesday"
+	}, {
+		code : 3,
+		description : "Wednesday"
+	}, {
+		code : 4,
+		description : "Thursday"
+	}, {
+		code : 5,
+		description : "Friday"
+	}, {
+		code : 6,
+		description : "Saturday"
+	} ]);
 
 })();
