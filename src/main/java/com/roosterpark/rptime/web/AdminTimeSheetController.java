7571401c0c15e131ef904912c5f06b452392cbb1
JJ Zabkar <jjzabkar@gmail.com>
2013-12-05 08:47:42 -0800

    Moved admin endpoints to `AdminTimeSheetController`. `AdminTimeSheetController` URLs: /admin/timesheet `TimeSheetController` URLs: /timesheet

---
+++ src/main/java/com/roosterpark/rptime/web/AdminTimeSheetController.java
@@ -0,0 +1,144 @@
+package com.roosterpark.rptime.web;
+
+import static org.springframework.http.HttpStatus.ACCEPTED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.google.appengine.api.users.UserService;
+import com.roosterpark.rptime.model.TimeSheet;
+import com.roosterpark.rptime.model.TimeSheetView;
+import com.roosterpark.rptime.model.Worker;
+import com.roosterpark.rptime.service.TimeSheetService;
+import com.roosterpark.rptime.service.WorkerService;
+
+/**
+ * {@link Controller} responsible for <b>Administrator-only</b> {@link TimeSheet} ({@link TimeSheetView})-related MVC endpoints of the form
+ * {@code /admin/timesheet}.
+ * <p>
+ * Per {@code web.xml}, Administrator-only endpoint {@link RequestMapping RequestMappings} are secured by having {@code /admin/} in their
+ * URL path.
+ * 
+ * @author jjzabkar
+ */
+@Controller
+@RequestMapping(value = "/admin/timesheet")
+public class AdminTimeSheetController {
+
+	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+	private static final String WORKER_MODEL_ATTRIBUTE_NAME = "worker";
+	private static final String CLIENT_ID_PARAMETER_NAME = "clientId";
+	private static final String WORKER_ID_PARAMETER_NAME = "workerId";
+
+	@Inject
+	TimeSheetService service;
+	@Inject
+	UserService userService;
+	@Inject
+	WorkerService workerService;
+
+	/**
+	 * Method interceptor that sets the {@code worker} {@link ModelAttribute} <i>prior</i> to invoking the {@link RequestMapping} handler
+	 * methods.
+	 * 
+	 * @return the {@link Worker} attribute attached to the {@code HttpServletRequest request} by {@code WorkerFilter}.
+	 */
+	@ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME)
+	public Worker initWorkerBeforeInvokingHandlerMethod(HttpServletRequest request) {
+		return (Worker) request.getAttribute(WORKER_MODEL_ATTRIBUTE_NAME);
+	}
+
+	@RequestMapping(value = "{id}", method = DELETE)
+	@ResponseStatus(NO_CONTENT)
+	public void deleteId(@PathVariable("id") Long id) {
+		service.delete(id);
+	}
+
+	@RequestMapping(method = GET)
+	@ResponseBody
+	public List<TimeSheetView> getAll() {
+		return service.getAllAdmin();
+	}
+
+	@RequestMapping(value = "client/{clientId}", method = GET)
+	@ResponseBody
+	public List<TimeSheetView> getAllForClientId(@PathVariable(CLIENT_ID_PARAMETER_NAME) final Long clientId) {
+		LOGGER.debug("getAll for clientId={}", clientId);
+		return service.getAllForClient(clientId);
+	}
+
+	@RequestMapping(value = "worker/{workerId}", method = GET)
+	@ResponseBody
+	public List<TimeSheetView> getAllForWorkerId(@PathVariable(WORKER_ID_PARAMETER_NAME) final Long workerId) {
+		LOGGER.debug("getAll for workerId={}", workerId);
+		return service.getAllForWorker(workerId);
+	}
+
+	@RequestMapping(value = "{id}", method = GET)
+	@ResponseBody
+	public TimeSheetView getId(@PathVariable Long id) {
+		return service.getById(id);
+	}
+
+	@RequestMapping(value = "new", method = GET)
+	@ResponseBody
+	public TimeSheetView getNew(@ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME) Worker worker) {
+		throw new IllegalArgumentException("IllegalArgumentException TBD");
+		// validateWorkerOrThrowWorkerNotFoundException(worker);
+		// return getTimeSheetForDate(worker, new LocalDate());
+	}
+
+	@RequestMapping(method = POST)
+	@ResponseBody
+	public TimeSheetView post(@RequestBody TimeSheetView item) {
+		LOGGER.debug("saving TimeSheetView {}", item);
+		TimeSheetView view = service.set(item);
+		return view;
+	}
+
+	/**
+	 * Set the {@code flagged} attribute for a {@link Worker} with id={@code id}.
+	 */
+	@RequestMapping(value = "flag/{id}/{flagged}", method = POST)
+	@ResponseBody
+	public void postIdFlagged(@PathVariable("id") Long id, @PathVariable("flagged") String flagged) {
+		LOGGER.debug("flag to {} timesheet {}", flagged, id);
+		Boolean b = Boolean.valueOf(flagged);
+		service.flag(id, b);
+	}
+
+	@RequestMapping(value = "submit/{id}", method = POST)
+	@ResponseBody
+	public void postSubmitId(@PathVariable("id") Long id) {
+		LOGGER.debug("submit timesheet {}", id);
+		service.submit(id);
+	}
+
+	@RequestMapping(value = "{id}", method = PUT)
+	@ResponseStatus(ACCEPTED)
+	@ResponseBody
+	public TimeSheetView putId(@PathVariable("id") Long id, @RequestBody TimeSheetView item) {
+		Validate.isTrue(item.getId().equals(id));
+		service.set(item);
+		return item;
+	}
+
+}
--- src/main/java/com/roosterpark/rptime/web/TimeSheetController.java
+++ src/main/java/com/roosterpark/rptime/web/TimeSheetController.java
@@ -1,11 +1,7 @@
 package com.roosterpark.rptime.web;
 
-import static org.springframework.http.HttpStatus.ACCEPTED;
-import static org.springframework.http.HttpStatus.NO_CONTENT;
-import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
-import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 import java.util.List;
 
@@ -22,7 +18,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.google.appengine.api.users.UserService;
 import com.roosterpark.rptime.exceptions.WorkerNotFoundException;
@@ -34,13 +29,11 @@ import com.roosterpark.rptime.service.WorkerService;
 
 /**
  * {@link Controller} responsible for {@link TimeSheet} ({@link TimeSheetView})-related MVC endpoints.
- * <p>
- * Per {@code web.xml} Administrator-only endpoint {@link RequestMapping RequestMappings} are secured by having {@code /admin/} in their URL
- * path.
  * 
  * @author jjzabkar
  */
 @Controller
+@RequestMapping(value = "/timesheet")
 public class TimeSheetController {
 	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 	private static final String WORKER_MODEL_ATTRIBUTE_NAME = "worker";
@@ -59,76 +52,11 @@ public class TimeSheetController {
 	 * @return the {@link Worker} attribute attached to the {@code HttpServletRequest request} by {@code WorkerFilter}.
 	 */
 	@ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME)
-	public Worker getWorkerBeforeInvokingHandlerMethod(HttpServletRequest request) {
+	public Worker initWorkerBeforeInvokingHandlerMethod(HttpServletRequest request) {
 		return (Worker) request.getAttribute(WORKER_MODEL_ATTRIBUTE_NAME);
 	}
 
-	@RequestMapping(value = "/admin/timesheet/new", method = { POST, GET })
-	@ResponseBody
-	public TimeSheetView getNew(@ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME) Worker worker) {
-		validateWorkerOrThrowWorkerNotFoundException(worker);
-		return getTimeSheetForDate(worker, new LocalDate());
-	}
-
-	@RequestMapping(value = "/admin/timesheet/flag/{id}/{flagged}", method = POST)
-	@ResponseBody
-	public void flag(@PathVariable("id") Long id, @PathVariable("flagged") String flagged) {
-		LOGGER.debug("flag to {} timesheet {}", flagged, id);
-		Boolean b = Boolean.valueOf(flagged);
-		service.flag(id, b);
-	}
-
-	@RequestMapping(value = "/admin/timesheet", method = GET)
-	@ResponseBody
-	public List<TimeSheetView> getAllAdmin() {
-		return service.getAllAdmin();
-	}
-
-	@RequestMapping(value = "/admin/timesheet/submit/{id}", method = POST)
-	@ResponseBody
-	public void submit(@PathVariable("id") Long id) {
-		LOGGER.debug("submit timesheet {}", id);
-		service.submit(id);
-	}
-
-	@RequestMapping(value = "/admin/timesheet/{id}", method = PUT)
-	@ResponseStatus(ACCEPTED)
-	@ResponseBody
-	public TimeSheetView put(@PathVariable("id") Long id, @RequestBody TimeSheetView item) {
-		Validate.isTrue(item.getId().equals(id));
-		service.set(item);
-		return item;
-	}
-
-	@RequestMapping(value = "/admin/timesheet/{id}", method = GET)
-	@ResponseBody
-	public TimeSheetView getByAdminId(@PathVariable Long id) {
-		return service.getById(id);
-	}
-
-	@RequestMapping(value = "/admin/timesheet/{id}", method = DELETE)
-	@ResponseStatus(NO_CONTENT)
-	public void delete(@PathVariable("id") Long id) {
-		service.delete(id);
-	}
-
-	@RequestMapping(value = "/admin/timesheet/client/{clientId}", method = GET)
-	@ResponseBody
-	public List<TimeSheetView> getAllForClientId(@PathVariable("clientId") final Long clientId) {
-		LOGGER.debug("getAll for clientId={}", clientId);
-		return service.getAllForClient(clientId);
-	}
-
-	@RequestMapping(value = "/admin/timesheet/worker/{workerId}", method = GET)
-	@ResponseBody
-	public List<TimeSheetView> getAllForWorkerId(@PathVariable("workerId") final Long workerId) {
-		LOGGER.debug("getAll for workerId={}", workerId);
-		return service.getAllForWorker(workerId);
-	}
-
-	// non-admin functions
-
-	@RequestMapping(value = "/timesheet", method = GET)
+	@RequestMapping(method = GET)
 	@ResponseBody
 	public List<TimeSheetView> getAll(@ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME) Worker worker) throws WorkerNotFoundException {
 		LOGGER.debug("ModelAttribute worker={}", worker);
@@ -136,14 +64,25 @@ public class TimeSheetController {
 		return service.getAllForWorker(id);
 	}
 
-	@RequestMapping(value = "/timesheet/current", method = { GET })
+	@RequestMapping(value = "current", method = GET)
 	@ResponseBody
 	public TimeSheetView getCurrent(@ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME) Worker worker) throws WorkerNotFoundException {
 		validateWorkerOrThrowWorkerNotFoundException(worker);
 		return getTimeSheetForDate(worker, new LocalDate());
 	}
 
-	@RequestMapping(value = "/timesheet/last", method = { GET })
+	@RequestMapping(value = "{id}", method = GET)
+	@ResponseBody
+	public TimeSheetView getId(@PathVariable Long id, @ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME) Worker worker) {
+		validateWorkerOrThrowWorkerNotFoundException(worker);
+		final Long workerId = worker.getId();
+		TimeSheetView result = service.getById(workerId);
+		LOGGER.debug("model workerId={}, timeSheet.workerId={}", workerId, result.getWorkerId());
+		// TODO: Validate.isTrue(workerId.equals(result.getWorkerId()),"Worker is only permitted to get ");
+		return result;
+	}
+
+	@RequestMapping(value = "last", method = GET)
 	@ResponseBody
 	public TimeSheetView getLast(@ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME) Worker worker) {
 		validateWorkerOrThrowWorkerNotFoundException(worker);
@@ -151,7 +90,15 @@ public class TimeSheetController {
 		return getTimeSheetForDate(worker, d);
 	}
 
-	@RequestMapping(value = "/timesheet/next", method = { GET })
+	@RequestMapping(value = "new/{date}", method = GET)
+	@ResponseBody
+	public TimeSheetView getNewDate(@PathVariable("date") String dateString, @ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME) Worker worker) {
+		validateWorkerOrThrowWorkerNotFoundException(worker);
+		LocalDate date = new LocalDate(dateString);
+		return getTimeSheetForDate(worker, date);
+	}
+
+	@RequestMapping(value = "next", method = GET)
 	@ResponseBody
 	public TimeSheetView getNext(@ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME) Worker worker) {
 		validateWorkerOrThrowWorkerNotFoundException(worker);
@@ -159,40 +106,13 @@ public class TimeSheetController {
 		return getTimeSheetForDate(worker, d);
 	}
 
-	@RequestMapping(value = "/timesheet/new/{date}", method = { POST, GET })
-	@ResponseBody
-	public TimeSheetView getDate(@PathVariable("date") String dateString, @ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME) Worker worker) {
-		validateWorkerOrThrowWorkerNotFoundException(worker);
-		LocalDate date = new LocalDate(dateString);
-		return getTimeSheetForDate(worker, date);
-	}
-
-	@RequestMapping(value = "/timesheet/{id}", method = GET)
-	@ResponseBody
-	public TimeSheetView getById(@PathVariable Long id, @ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME) Worker worker) {
-		validateWorkerOrThrowWorkerNotFoundException(worker);
-		final Long workerId = worker.getId();
-		TimeSheetView result = service.getById(workerId);
-		LOGGER.debug("model workerId={}, timeSheet.workerId={}", workerId, result.getWorkerId());
-		// Validate.isTrue(workerId.equals(result.getWorkerId()),"Worker is only permitted to get ");
-		return result;
-	}
-
-	@RequestMapping(value = "/timesheet", method = POST)
+	@RequestMapping(method = POST)
 	@ResponseBody
 	public TimeSheetView post(@RequestBody TimeSheetView item, @ModelAttribute(WORKER_MODEL_ATTRIBUTE_NAME) Worker worker) {
 		validateWorkerOrThrowWorkerNotFoundException(worker);
 		final Long workerId = worker.getId();
 		LOGGER.debug("saving TimeSheetView {}", item);
 		Validate.isTrue(workerId.equals(item.getWorkerId()));
-		TimeSheetView view = service.set(item);
-		return view;
-	}
-
-	@RequestMapping(value = "/admin/timesheet", method = POST)
-	@ResponseBody
-	public TimeSheetView postAdmin(@RequestBody TimeSheetView item) {
-		LOGGER.debug("saving TimeSheetView {}", item);
 		TimeSheetView view = service.set(item);
 		return view;
 	}
